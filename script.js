fa4ad57a8ce637571b226595d09ec002063d0235
Ivane Tvildiani <ivane.tvildiani.1@btu.edu.ge>
2023-01-24 11:09:05 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,75 @@
+// homeWork-15 / ivane_tvildiani
+
+
+/* 
+(_________1___________)
+*/
+
+// var number = 100;
+
+// for (let i = 0; i <= number; i++) {
+//     console.log(i);
+// }
+
+/* 
+(_________2___________)
+*/
+
+// let i = 0;
+// let n = 50;
+
+// while (i <= n) {
+//     console.log(i);
+//     i += 1;
+// }
+
+/* 
+(_________3___________)
+*/
+
+// let i = 0;
+// let k = 150;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while(i <= k);
+
+/* 
+(_________4___________)
+*/
+
+// var arrayCars = [];
+
+// let i = 1;
+// let n = 100;
+
+// while (i <= n) {
+//     arrayCars.push(i*n);
+//     i += 1;
+// }
+
+// console.log(arrayCars);
+
+// ვერ მივხვდი ზუსტად რა უნდა გაგვეკეთებინა
+
+/* 
+(_________5___________)
+*/
+
+// let arrayNames = ["Guranda", "Erekle", "Anano", "Buba", "Cici",];
+
+// console.log("Default - names ",arrayNames);
+
+// arrayNames.shift();
+// console.log("Deleted first name", arrayNames);
+
+// arrayNames.unshift("Mariami");
+// console.log("Added name in start", arrayNames);
+
+// arrayNames.pop();
+// arrayNames.push("Irakli")
+// console.log("Added name in end", arrayNames);
+
+
+
